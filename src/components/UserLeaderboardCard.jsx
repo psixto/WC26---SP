2701a6e7fd0686fd95836b3dfefb73663fac3591
Gonzalo Balay Losada <gbalay@denodo.com>
2026-02-13 10:26:28 +0100

    implementing userCards

--- src/components/UserLeaderboardCard.jsx
+++ src/components/UserLeaderboardCard.jsx
@@ -3,9 +3,7 @@ import styles from './UserLeaderboardCard.module.css'
 export function UserLeaderboardCard({ user }) {
     return (
         <div className={styles.userCard}>
-            <div className={styles.userInfo}>
-                <span>{user.username}</span>
-                <span>{user.points}</span>
-            </div>
+                <h2>{user.username}</h2>
+                <h3>{user.points}</h3>
         </div>
 )}
